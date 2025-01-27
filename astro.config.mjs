7bfb736cd611a0d7d9d1696103b054f3c5caf693
Léa <apothelozlea2@gmail.com>
2025-01-27 08:49:21 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,15 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://jobtrek.github.io/', // TODO: Change to your github url
-    base: 'progress-report', // TODO: Change to your repo name
+    site: 'https://lea-apotheloz/raporte-de-formation/', // TODO: Change to your github url
+    base: 'raporte-de-formation', // TODO: Change to your repo name
     integrations: [starlight({
         title: 'Progress  report',
         social: {
-            github: 'https://github.com/jobtrek/progress-report', // TODO: Change to your repo url
+            github: 'https://github.com/lea-apotheloz/raporte-de-formation', // TODO: Change to your repo url
         },
         editLink: {
-            baseUrl: 'https://github.com/jobtrek/progress-report/edit/main/docs/', // TODO: before /edit, change to your repo url
+            baseUrl: 'https://github.com/lea-apotheloz/raporte-de-formation/edit/main/docs/', // TODO: before /edit, change to your repo url
         },
         customCss: [
             // Relative path to your custom CSS file
